401802aa07f30a59ec310026ec3285d863912d00
Shail <sbrixey9@gmail.com>
2025-05-18 16:29:59 -0700

    5/18/25

--- src/_02_loop_variables/_1_years_alive/YearsAlive.java
+++ src/_02_loop_variables/_1_years_alive/YearsAlive.java
@@ -1,11 +1,17 @@
 package _02_loop_variables._1_years_alive;
 
+import javax.swing.JOptionPane;
+
 public class YearsAlive {
 
 	public static void main(String[] args) {
 		
-		
-		
+		String input = JOptionPane.showInputDialog("What year were you born?");
+		int now = 2025;
+		int year = Integer.parseInt(input);
+		for(int i=year; i <= now; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
