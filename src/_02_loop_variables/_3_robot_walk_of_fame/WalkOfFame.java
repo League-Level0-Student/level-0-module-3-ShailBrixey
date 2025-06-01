309d9d9c29a4112cf9ec7e1d4f9d63845c9f0ada
Shail <sbrixey9@gmail.com>
2025-06-01 15:18:24 -0700

    6/1/25

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -5,13 +5,14 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot rob = new Robot("mini");
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-		Robot.
+		rob.setX(55);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		rob.penDown();
+		rob.setSpeed(15);
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
@@ -20,10 +21,16 @@ public class WalkOfFame {
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
-		i=0;
-		for(i=0; i>=9; i++) {
-			
+		for(int h=0; h<=10; h++) {
+		for(int i=0; i<=5; i++) {
+			rob.move(30);
+			rob.turn(144);
 		}
+		rob.penUp();
+		rob.setAngle(90);
+		rob.move(50);
+		rob.penDown();
+	}
 	}
 
 }
